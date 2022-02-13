646418ebfa79d2e54b0210539acf200c827b5239
123yash8 <60549277+123yash8@users.noreply.github.com>
2022-02-13 19:53:44 +0530

    Update run-xmrig.sh

--- run-xmrig.sh
+++ run-xmrig.sh
@@ -1,7 +1,9 @@
-sudo apt update && apt-get install git build-essential cmake automake libtool autoconf -y
+sudo apt update && apt-get install git build-essential cmake automake libtool autoconf tmux -y
  git clone https://github.com/xmrig/xmrig.git
  mkdir xmrig/build && cd xmrig/scripts
  ./build_deps.sh && cd ../build
  cmake .. -DXMRIG_DEPS=scripts/deps
  make -j$(nproc)
+ tmux
+ #change your wallet address  between ":" and"."
 /root/xmrig/build/./xmrig -o rx.unmineable.com:3333 -u TRX:TYrw2FwmFpqzzxTs96Qpm59z42s23S6vVV.5red#3c09-faja -p x
