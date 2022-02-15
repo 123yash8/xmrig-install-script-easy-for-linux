cbe061c98e8d6f0035c4fc0789d826e9c2bc0a3c
123yash8 <60549277+123yash8@users.noreply.github.com>
2022-02-15 16:28:06 +0530

    Update run-xmrig.sh

--- run-xmrig.sh
+++ run-xmrig.sh
@@ -1,3 +1,12 @@
+tmux
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+YELLOW="\e[33m"
+########
+echo -e "${RED}Tool made by yash Deshmukh ${ENDCOLOR}" 
+echo -e "${GREEN} follow on instagram www.instagram.com/yashdeshmukh01_07_04${ENDCOLOR}"
+echo -e "${YELLOW}https://github.com/123yash8/xmrig-install-script-easy-for-linux/${ENDCOLOR}"
 sudo apt update  
 sudo apt-get install git build-essential cmake automake libtool autoconf tmux -y
 cd
@@ -7,7 +16,12 @@ sudo unset http_proxy
  ./build_deps.sh && cd ../build
  cmake .. -DXMRIG_DEPS=scripts/deps
  make -j$(nproc)
- 
 cd xmrig/build
 chmod + x *
-./xmrig -o rx.unmineable.com:3333 -u TRX:TYrw2FwmFpqzzxTs96Qpm59z42s23S6vVV.6red#3c09-faja -p x
+echo Your coin name please:-
+read coin
+echo your wallet address:-
+read wallet
+echo your worker name (put any no):-
+read work
+./xmrig -o rx.unmineable.com:3333 -u $coin:$wallet.$work#3c09-faja -p x
